19dabf74fdb044bc1c6296747c41a63e36e4afa6
Jack Thomas <168010304+jackthomas1430@users.noreply.github.com>
2024-06-03 09:58:53 -0600

    Add files via upload

---
+++ python_challenge/PyBank/pybank_main.py
@@ -0,0 +1,74 @@
+import os
+import csv
+
+# Path to collect data 
+budget_csv = os.path.join('Resources', 'budget_data.csv')
+
+# Initialize total months + profit losses variables
+total_months = 0
+total = 0
+#Track previous profit/loss
+previous_change = None
+#Initialize lists to track monthly profit/loss change and corresponding month
+changes = []
+months = []
+#Initialize dictionaries to track greatest increase and decrease
+#Convert amount value to float 
+greatest_increase = {'date': None, 'amount': float()}
+greatest_decrease = {'date': None, 'amount': float()}
+
+# Open the CSV file and store header row 
+with open(budget_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+    
+ ##Loop through each row in csv; dates in first column, profits in the second column 
+    for row in csvreader:
+        date = row[0]
+        profit = int(row[1])
+##Calculate total months and total net profit
+        total_months += 1
+        total += int(profit)
+        
+        #Check that the previous change has a value to ensure that the loop starts at the first change value and ends at the last
+        # Calculate monthly changes; add change to changes list and corresponding dates to months list
+        if previous_change is not None:
+            change = profit - previous_change
+            changes.append(change)
+            months.append(date)
+            
+            # Calculate greatest increase
+            #If the current change is greater than the current greatest increase, the greatest_increase list now = the current amount and date
+            if change > greatest_increase['amount']:
+                greatest_increase['amount'] = change
+                greatest_increase['date'] = date
+            
+            # Calculate greatest decrease
+            if change < greatest_decrease['amount']:
+                greatest_decrease['amount'] = change
+                greatest_decrease['date'] = date
+        
+        # Update previous_change to continue loop
+        previous_change = profit
+
+# Calculate the average change
+average_change = sum(changes) / len(changes)
+
+#The results
+results = (
+    "\n"
+    "Financial Analysis\n\n"
+    "----------------------------\n\n"
+    f"Total Months: {total_months}\n\n"
+    f"Total: ${total}\n\n"
+    f"Average Change: ${average_change:.2f}\n\n"
+    f"Greatest Increase in Profits: {greatest_increase['date']} (${greatest_increase['amount']})\n\n"
+    f"Greatest Decrease in Profits: {greatest_decrease['date']} (${greatest_decrease['amount']})\n"
+)
+
+# Print the results to the terminal
+print(results)
+
+#Export the results to a text file
+with open('financial_analysis.txt', 'w') as file:
+    file.write(results)
---
+++ python_challenge/PyPoll/pypoll_main.py
@@ -0,0 +1,62 @@
+import os
+import csv
+
+# Path to collect data 
+election_csv = os.path.join('Resources', 'election_data.csv')
+
+# Initialize variables for total number of votes, winner, and most votes
+# Create dictionary to store election results by candidate
+total_votes = 0
+election_results = {}
+winner = ""
+most_votes = 0
+
+#Open CSV file and store header 
+with open(election_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header=next(csvreader)
+
+ # For loop through to calculate votes; add up total votes; candidate name stored in column 3 
+    for row in csvreader:
+        total_votes += 1
+        candidate = row[2]
+        #Check if candidate is already in election results dictionary and add to vote count or sets vote count to 1 
+        if candidate in election_results:
+            election_results[candidate] += 1
+        else:
+            election_results[candidate] = 1
+
+    # Create string to print total votes result
+results = (
+        "\n"
+        f"Election Results\n\n"
+        f"-------------------------\n\n"
+        f"Total Votes: {total_votes}\n\n"
+        f"-------------------------\n\n")
+    
+#Calculate the winner
+#Loop through every candidate + vote count in election_results dictionary 
+#Calculate percentage of votes each cadidate received
+#Add candidate name, % votes, and total votes to results
+for candidate, votes in election_results.items():
+    percentage = (votes / total_votes) * 100
+    results += f"{candidate}: {percentage:.3f}% ({votes})\n\n"
+    #Calculate most votes
+    #Check if candidate has more votes than the max recorded
+    #Update winner and most votes
+    if votes > most_votes:
+            winner = candidate
+            most_votes = votes
+# Add the winner to results
+results += (
+    f"-------------------------\n\n"
+    f"Winner: {winner}\n\n"
+    f"-------------------------\n\n"
+)
+
+# Print results to terminal
+print(results)
+
+# Export to text file
+with open("election_results.txt", "w") as file:
+    file.write(results)
